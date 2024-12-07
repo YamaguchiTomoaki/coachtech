f3432b17f032fccf0002459d9f889654ddeb6a0d
yamaguchi tomoaki <t.yamaguchi@en-gineer.com>
2024-12-08 00:11:23 +0900

    商品表示(仮)

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Item;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Favorite;
+
 
 class ItemController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $items = Item::with('user', 'condition', 'category')->get();
+        $itemsCount = count($items);
+        $itemsArray = $items->toArray();
+
+        $user = Auth::user();
+        if ($user != null) {
+            $mylists = Favorite::where([
+                ['user_id', '=', $user['id']],
+            ])->orderBy('id')->get();
+            $mylistsCount = count($mylists);
+            $mylistsArray = $mylists;
+        } else {
+            $mylistsArray = null;
+            $mylistsCount = null;
+        }
+
+
+        /*for ($id = 0; $id < $itemsCount; $id++) {
+            $categoryCount = count($itemsArray[$id]['category']);
+        }
+        $categoryCount = count($itemsArray[4]['category']);
+        dd($categoryCount);*/
+        return view('index', compact('itemsArray', 'itemsCount', 'mylistsArray', 'mylistsCount'));
     }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,4 +5,20 @@
 @endsection
 
 @section('content')
+<div class="index__header">
+    <input type="radio" name="item" value="recommendation" id="recommendation" checked>
+    <label for="recommendation">おすすめ</label>
+    <input type="radio" name="item" value="mylist" id="mylist">
+    <label for="mylist">マイリスト</label>
+</div>
+<div class="index__content">
+    <div class="recommendation__content" id="recommendation">
+        @for ($id=0;$id<$itemsCount;$id++)
+            <div class="item__card">
+            <img src="{{ asset('storage/' . $itemsArray[$id]['image']) }}" alt="">
+            <p class="item__name">{{ $itemsArray[$id]['name'] }}</p>
+    </div>
+    @endfor
+</div>
+</div>
 @endsection
